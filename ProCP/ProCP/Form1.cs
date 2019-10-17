1a22c4971713ce446540971d6bd5fdc3bb22b8b2
Nidhi Sharma <i380096.hera.fhict.nl>
2019-10-17 20:12:38 +0200

    charts added

--- ProCP/ProCP/Form1.cs
+++ ProCP/ProCP/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using System.Timers;
+using LiveCharts.Wpf;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace ProCP
 {
@@ -15,6 +21,40 @@ namespace ProCP
         public Form1()
         {
             InitializeComponent();
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Time",
+                    Values = new ChartValues<int> {10, 25, 32, 45, 55},
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title = "Flight Number",
+                Labels = new[] { "1", "2", "3", "4", "5" }
+            });
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Transfer Time",
+                LabelFormatter = value => value.ToString()
+            });
+
+            cartesianChart1.LegendLocation = LegendLocation.Right;
+
+            //modifying any series values will also animate and update the chart
+
+
+            cartesianChart1.DataClick += CartesianChart1OnDataClick;
+        }
+
+        private void CartesianChart1OnDataClick(object sender, ChartPoint chartPoint)
+        {
+            MessageBox.Show("You clicked (" + chartPoint.X + "," + chartPoint.Y + ")");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,6 +63,11 @@ namespace ProCP
             this.Size = Screen.PrimaryScreen.WorkingArea.Size;
             FormBorderStyle = FormBorderStyle.Sizable;
             WindowState = FormWindowState.Maximized;
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
