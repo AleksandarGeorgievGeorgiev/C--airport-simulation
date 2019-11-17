fc53ec5f8a6f99bab6e0d9b32762d21ea79901b7
nguyen quoc <nguyenquoc080298@gmail.com>
2019-11-17 13:21:02 +0100

    read from binary file

---
+++ ProCP/ProCP/Data/ReadFromImageFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProCP.Data
+{
+    class ReadFromImageFile: IData
+    {
+
+        public void ReadData(string file)
+        {
+            try
+            {
+                Stream stream = new FileStream(file, FileMode.Open, FileAccess.Read);
+                IFormatter formatter = new BinaryFormatter();
+                // deserialize obj
+            }
+            catch(IOException e)
+            {
+                MessageBox.Show(e);
+            }
+        }
+    }
+}
