e0b62d6df1872fd8ab7a0f2d80762fd44bf9a80a
Rozalina <rozituu@gmail.com>
2019-11-26 15:07:17 +0100

    Modles1

---
+++ ProCP/ProCP/Visuals/DropOffTile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ProCP.Nodes;
+using ProCP.Visuals;
+
+namespace ProCP.Visuals
+{
+    class DropOffTile : GridTile 
+    {
+        public DropOffTile(int column, int row, int tileWidth, int tileHeight) : base(column, row, tileWidth, tileHeight)
+        {
+            this.Column = column;
+            this.Row = row;
+
+            clickableColor = Brushes.White;
+            unclickableColour = Brushes.LightGray;
+
+            imgpath = "/../../Resources/luggage-cart.png";
+            img = loadImage(imgpath, img, tileWidth, tileHeight);
+
+            fillBrush = clickableColor;
+        }
+        protected override void DrawTileInfo(Graphics g, RectangleF r, int tileHeight)
+        {
+            Font stringFont = new Font("Ariel", tileHeight / 4, FontStyle.Bold, GraphicsUnit.Pixel);
+            DropOff c = nodeInGrid as DropOff;
+            string gateID = Convert.ToString(c.NodeId);
+            g.DrawString("Gate: " + gateID, stringFont, Brushes.Black, r);
+        }
+
+    }
+}
---
+++ ProCP/ProCP/Visuals/SecurityTile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ProCP.Visuals;
+
+
+namespace ProCP.Visuals
+{
+     public class SecurityTile : GridTile
+    {
+        public SecurityTile(int column, int row, int tileWidth, int tileHeight) : base(column, row, tileWidth, tileHeight)
+        {
+            this.Column = column;
+            this.Row = row;
+
+            clickableColor = Brushes.White;
+            unclickableColour = Brushes.LightGray;
+
+            imgpath = "/../../Resources/scan.png";
+            img = loadImage(imgpath, img, tileWidth, tileHeight);
+
+            fillBrush = clickableColor;
+        }
+    }
+}
